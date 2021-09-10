5bbe49a92249847b197e564c6e9e9a55253b9d0d
seasameoil <whdwlgus12@gmail.com>
2021-09-10 19:43:10 +0900

    2021/09/10(금)

--- src/components/App/Profile.jsx
+++ src/components/App/Profile.jsx
@@ -1,35 +1,14 @@
 import React from "react";
-import { Link, Route, BrowserRouter } from "react-router-dom";
 
-import "./assets/css/Auth.scss";
+import "./assets/css/Profile.scss";
 
-function AuthWrapper() {
-  //SNS 연동은 Client ID 발급 받으면 생성 예정
+function Profile() {
   return (
-    <div className="container">
-      <header className="welcom-header">
-        <h1>Welcom to PROJECT</h1>
-        <p>계정을 가지고 있다면 이메일로 로그인 해주세요 :)</p>
-      </header>
-
-      <form id="login-form">
-        <input name="id" type="email" placeholder="이메일을 입력해주세요" />
-        <input
-          name="password"
-          type="password"
-          placeholder="비밀번호를 입력해주세요"
-        />
-        <input type="submit" value="Log In" />
-
-        <div className="help-login">
-          <a href="#">아이디/비밀번호 찾기</a>
-          <Link to="/Register">회원가입</Link>
-        </div>
-      </form>
-
-      <div></div>
+    <div>
+      <span>비밀번호 변경</span>
+      <span>회원 탈퇴</span>
     </div>
   );
 }
 
-export default AuthWrapper;
+export default Profile;
