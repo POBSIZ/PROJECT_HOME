79f38d5b240ae130ed144be9e704a5e2cc01b4c6
seasameoil <whdwlgus12@gmail.com>
2021-09-10 21:54:21 +0900

    ----

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
+import React from "react";
+import { hot } from "react-hot-loader";
 import { Link, Route, Switch } from "react-router-dom";
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
-import './assets/css/style.scss';
+import "./assets/css/style.scss";
 
-import Home from '../pages/home/home';
+import Home from "../pages/home/home";
 
-import Login from '../pages/auth/login/login';
-import Register from '../pages/auth/register/Register';
-import Profile from '../pages/auth/profile/Profile';
+import Login from "../pages/auth/login/login";
+import Register from "../pages/auth/register/Register";
+import Profile from "../pages/auth/profile/Profile";
 
 const Main = () => {
-    return (
-        <main className="main">
-            <Switch>
-                <Route exact path="/" component={Home}></Route>
+  return (
+    <main className="main">
+      <Switch>
+        <Route exact path="/" component={Home}></Route>
 
-                <Route exact path="/login" component={Login}></Route>
-                <Route exact path="/register" component={Register}></Route>
-                <Route exact path="/profile" component={Profile}></Route>
-
-            </Switch>
-        </main>             
-    )
-}
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/register" component={Register}></Route>
+        <Route exact path="/profile" component={Profile}></Route>
+      </Switch>
+    </main>
+  );
+};
 
 export default hot(module)(Main);
