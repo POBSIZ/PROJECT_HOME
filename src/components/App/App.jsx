ea7d538a58a85470c0bde1362986c7fa9a0a2ba2
seasameoil <whdwlgus12@gmail.com>
2021-09-10 19:40:12 +0900

    2021/09/10(금)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Link, Route, BrowserRouter } from "react-router-dom";
-import { hot } from 'react-hot-loader';
-import PropTypes from 'prop-types';
-import { Helmet } from 'react-helmet';
+import { hot } from "react-hot-loader";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
 
-import './assets/css/App.scss';
+import AuthWrapper from "./Auth";
+import Register from "./Register";
+import Profile from "./Profile";
 
-class App extends React.Component {
-    render() {
-        return (
-            <div className="main">
-                <h1>Webpack with React</h1>
-            </div>             
-        );
-    }
+function App() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact={true} component={AuthWrapper} />
+      <Route path="/Register" component={Register} />
+      <Route path="/Profile" component={Profile} />
+    </BrowserRouter>
+  );
 }
 
 export default hot(module)(App);
