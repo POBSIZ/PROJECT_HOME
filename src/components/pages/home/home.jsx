2e3d9f150c0d6ef88c21ccdc35a23e4a66752275
seasameoil <whdwlgus12@gmail.com>
2021-09-11 22:47:25 +0900

    2021/09/11
    -FAQ 부분
    - Contact 부분 추가

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -2,25 +2,16 @@ import React from "react";
 import { hot } from "react-hot-loader";
 import { Link, Route, BrowserRouter } from "react-router-dom";
 
+import "./assets/css/style.scss";
+
 function Home() {
   //SNS 연동은 Client ID 발급 받으면 생성 예정
   return (
     <div className="home">
-      <div></div>
-      <div>
-        <span>F.A.Q</span>
-        <span>Q</span>
-        <div>
-          <span>hello</span>
-        </div>
-        <span>A</span>
-        <div>
-          <span>hi</span>
-        </div>
-      </div>
-      <div>
-        <span>Contact</span>
-      </div>
+      <div className="about"></div>
+      <div className="project"></div>
+      <div className="faq"></div>
+      <div className="contact"></div>
     </div>
   );
 }
