97b41dfc4f12cba4da42094f5c15b13e856cc45a
slbin-park <smpts00@naver.com>
2021-09-13 13:30:44 +0900

    Add main.jsx activity route

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -12,17 +12,17 @@ import Login from "../pages/auth/login/login";
 import Register from "../pages/auth/register/Register";
 import Profile from "../pages/auth/profile/Profile";
 import Find from "../App/Find";
-
+import Activity from "../pages/activity/main"
 const Main = () => {
   return (
     <main className="main">
       <Switch>
         <Route exact path="/" component={Home}></Route>
-
         <Route exact path="/login" component={Login}></Route>
         <Route exact path="/register" component={Register}></Route>
         <Route exact path="/profile" component={Profile}></Route>
         <Route exact path="/find" component={Find}></Route>
+        <Route path="/activity" component={Activity}/>
       </Switch>
     </main>
   );
