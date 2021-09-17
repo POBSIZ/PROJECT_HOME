752106ebbe26cb57425b2017fad400c948ee9629
luanaeun <tksop59@naver.com>
2021-09-18 01:15:37 +0900

    공지 삭제 기능

--- src/components/pages/notice/detail/NoticeDetail.jsx
+++ src/components/pages/notice/detail/NoticeDetail.jsx
@@ -1,16 +1,46 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
-import { Route, Link, Switch } from 'react-router-dom';
+import { Route, Link, Switch, useHistory } from 'react-router-dom';
 import './scss/NoticeDetail.scss';
-
-import testImg from '../noticemain/assets/noticeImage.png';
 
 
 export default function NoticeDetail({location}) {
 
+  const history = useHistory();
   let data = location.state;
+  let dataId = data.id
 
-  const file = 'http://3.35.43.53' + `${data.thumbnail}`
+  const thumbnail = 'http://3.35.43.53' + `${data.thumbnail}`
   let create_date = (data.created_date).substring(0,10);
+
+  const token = localStorage?.getItem('access_token');
+  
+  const config = {
+    headers: {"Authorization": `JWT ${token}`}
+  }
+
+  const deleteFunc = () => {
+    if (confirm('정말 삭제하시겠습니까?')) {
+      axios.delete(`http://3.35.43.53/api/v1/board/1/post/${dataId}`,
+        {
+          headers:{
+            Authorization: `jwt ${token}`,
+            'Content-Type': 'application/json'
+          }
+        })
+      .then((res) => { ()=> {
+        alert("삭제되었습니다."); 
+        }})
+      .catch((err) => {console.log("삭제 실패")})
+
+      history.push('/notice'); 
+      location.reload();
+      
+    } else {
+      console.log('삭제 취소');
+    }
+  }
+
 
   return(
     <div className="container">
@@ -21,23 +51,24 @@ export default function NoticeDetail({location}) {
       </div>
 
       <div className="modifyContainer">
-        <div>수정</div>
-        <div>삭제</div>
+        {/* <button >수정</button> */}
+        <button onClick={()=>deleteFunc()}>삭제</button>
       </div>
         
 
       <div className="contentContainer">
         <div className="imageContainer">
-          <img src={file}/>
+          <img src={thumbnail }/>
         </div>
 
         <div className="textContainer">
           <h1>{data.title}</h1>
 
           <div>작성자 {data.creator}</div>
-          <div>작성일 {data.create_date}</div>
+          <div>작성일 {create_date}</div>
           
           <div>{data.content}</div>
+          <div>파일{data.upload_file}</div>
         </div>
 
       </div>
